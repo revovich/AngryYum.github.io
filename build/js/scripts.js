9c795da8bcd0612c7905cf657ef42c892dc8b8ea
AngryYum <41231767+AngryYum@users.noreply.github.com>
2019-08-10 19:05:05 +0300

    Add files via upload

---
+++ build/js/scripts.js
@@ -0,0 +1,20 @@
+$(function () {
+
+	// var options = { videoId: '71WHYoqic2Y' };
+	// $('header').tubular(options);
+
+	$('.mini-menu').click(function(){
+		$(this).toggleClass('mini-menu-active');
+		$('.menu').toggleClass('menu-active')
+	});
+	$('.product-desc a').hover(function(){
+		$(this).toggleClass('line_active');
+  });
+	$(".menu a").click(function (e) {
+		e.preventDefault();
+		$(".menu").slideUp();
+		let href = $(this).attr("href");
+		$("html,body").animate({ scrollTop: $(href).offset().top }, 1000);
+		
+	})
+});
