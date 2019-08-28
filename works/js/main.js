fed83f18957f395fa8abd521484fb376b60e2815
Taras Shulga <AngryYum@gmail.com>
2019-08-29 02:18:38 +0400

    qwerty

---
+++ works/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  if(document.querySelector('.home_slider')){
+    $(document).ready(function(){
+      $('.home_slider').slick({
+        slidesToShow: 1,
+        dots: true,
+        responsive: [
+          {
+            breakpoint: 900,
+            settings: {
+              arrows: false,
+                autoplay: true,
+           autoplaySpeed: 3000,
+            }
+          }]
+      });
+});
+}});
+$('.nav_btn').click(function(){
+  $('nav').toggleClass('nav_btn_active');
+  $('.nav_btn_line').toggleClass('nav_btn_line_active');
+});
+$('.news_wrap_btn').click(function(){
+  $('.sidebar_wrap').toggleClass('sidebar_wrap_active');
+  $('.sidebar').toggleClass('sidebar_wrap_active');
+  $(this).toggleClass('news_wrap_btn_active')
+
+})
