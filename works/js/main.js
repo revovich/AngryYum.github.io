689445ce3b12bfac18d28a8174621969e133a4d2
Taras Shulga <AngryYum@gmail.com>
2019-08-28 20:05:54 +0400

    new

--- works/js/main.js
+++ works/js/main.js
@@ -5,10 +5,15 @@ $(document).ready(function(){
         slidesToShow: 1,
         dots: true,
       });
-    });
-  }
-  
-  $('.subMenu').hover(function(){
-    $(this).fadeOut(500)
-  });
+});
+}});
+$('.nav_btn').click(function(){
+  $('nav').toggleClass('nav_btn_active');
+  $('.nav_btn_line').toggleClass('nav_btn_line_active');
+});
+$('.news_wrap_btn').click(function(){
+  $('.sidebar_wrap').toggleClass('sidebar_wrap_active');
+  $('.sidebar').toggleClass('sidebar_wrap_active');
+  $(this).toggleClass('news_wrap_btn_active')
+
 })
