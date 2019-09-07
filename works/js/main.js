ef07430e00bcc34736b953f94ac0be44d5a0446c
Taras Shulga <AngryYum@gmail.com>
2019-09-07 20:08:46 +0400

    yumy

--- works/js/main.js
+++ works/js/main.js
@@ -98,10 +98,9 @@ if(document.querySelector('.wow')){
     {
       boxClass:     'wow',      // animated element css class (default is wow)
       animateClass: 'animated', // animation css class (default is animated)
-      offset:       15,          // distance to the element when triggering the animation (default is 0)
+      offset:       170,          // distance to the element when triggering the animation (default is 0)
       mobile:       false,       // trigger animations on mobile devices (default is true)
-      live:         true, 
-      delay:        20,      // act on asynchronously loaded content (default is true)
+      live:         true,     // act on asynchronously loaded content (default is true)
       callback:     function(box) {
         // the callback is fired every time an animation is started
         // the argument that is passed in is the DOM node being animated
