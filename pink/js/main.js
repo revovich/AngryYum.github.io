4a5c05b91fc2a1f745bdec11b52143f5256f4d3b
AngryYum <41231767+AngryYum@users.noreply.github.com>
2020-01-23 08:27:36 +0200

    yumy

--- pink/js/main.js
+++ pink/js/main.js
@@ -1,167 +1,188 @@
-let burger = document.querySelector('.burger-wrap');
+window.onload = function() {
+  let burger = document.querySelector('.burger-wrap');
 
-burger.onclick = function(){
-  this.classList.toggle('burger-wrap-active');
-  document.querySelector('.burger-line').classList.toggle('burger-active');
-  document.querySelector('.header-pink').classList.toggle('header-pink-active');
- let lengBtn = document.querySelectorAll('.header-pink-btn');
-  for(i = 0; i<lengBtn.length; i++){
-    lengBtn[i].classList.toggle('header-pink-btn-active')
-  }
-}
-var mySwiper = new Swiper('.swiper-one', {
-  speed: 400,
-  spaceBetween: 100,
-  navigation: {
-    nextEl: '.swiper-one-next',
-    prevEl: '.swiper-one-prev',
-  },
-  pagination: {
-    el: '.swiper-one-pagination',
-    type: 'progressbar',
-  },
-});
-var mySwiper = new Swiper('.swiper-three', {
-  speed: 400,
-  spaceBetween: 30,
-  slidesPerView: 5,
-  pagination: {
-    el: '.swiper-three-pagination',
-    type: 'progressbar',
-  },
-  breakpoints: {
-   725: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-    1: {
-      slidesPerView: 3,
-      spaceBetween: 20
+  burger.onclick = function(){
+    this.classList.toggle('burger-wrap-active');
+    document.querySelector('.burger-line').classList.toggle('burger-active');
+    document.querySelector('.header-pink').classList.toggle('header-pink-active');
+   let lengBtn = document.querySelectorAll('.header-pink-btn');
+    for(i = 0; i<lengBtn.length; i++){
+      lengBtn[i].classList.toggle('header-pink-btn-active')
     }
   }
-});
-var galleryThumbs = new Swiper('.gallery-thumbs-one', {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-  direction: 'vertical',
-  slideActiveClass: 'gallery-thumbs-one-active',
-
-});
-var galleryTop = new Swiper('.gallery-top-one', {
-  spaceBetween: 10,
-  effect: 'fade',
-
-  fadeEffect: {
-    crossFade: true
-  },
-  navigation: {
-    nextEl: '.swiper-button-women-next',
-    prevEl: '.swiper-button-women-prev',
-  },
-  thumbs: {
-    swiper: galleryThumbs
-  }
-});
-let inp = document.querySelector('#phone');
-inp.addEventListener('focus', _ => {
-  if(!/^\+\d*$/.test(inp.value))
-    inp.value = '+380';
-});
-
-inp.addEventListener('keypress', e => {
-  if(!/\d/.test(e.key))
-    e.preventDefault();
-});
-var swiper = new Swiper('.swiper-about', {
-  direction: 'vertical',
-  slidesPerView: 'auto',
-  freeMode: true,
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-  mousewheel: true,
-});
-
-$('select').each(function(){
-  var $this = $(this), numberOfOptions = $(this).children('option').length;
-
-  $this.addClass('select-hidden'); 
-  $this.wrap('<div class="select"></div>');
-  $this.after('<div class="select-styled"></div>');
-
-  var $styledSelect = $this.next('div.select-styled');
-  $styledSelect.text($this.children('option').eq(0).text());
-
-  var $list = $('<ul />', {
-      'class': 'select-options'
-  }).insertAfter($styledSelect);
-
-  for (var i = 0; i < numberOfOptions; i++) {
-      $('<li />', {
-          text: $this.children('option').eq(i).text(),
-          rel: $this.children('option').eq(i).val()
-      }).appendTo($list);
+  let w = window.innerWidth;
+  if(document.querySelector('.card-blog-left .nav-page')){
+    if (w < 1080){
+      let title = document.querySelector('.card-blog-left .nav-page'); 
+      let top = document.querySelector('.card-blog-left-title'); 
+      document.querySelector('.new-title').appendChild(title).appendChild(top)
+    }
   }
 
-  var $listItems = $list.children('li');
-
-  $styledSelect.click(function(e) {
-      e.stopPropagation();
-      $('div.select-styled.active').not(this).each(function(){
-          $(this).removeClass('active').next('ul.select-options').hide();
-      });
-      $(this).toggleClass('active').next('ul.select-options').toggle();
-  });
-
-  $listItems.click(function(e) {
-      e.stopPropagation();
-      $styledSelect.text($(this).text()).removeClass('active');
-      $this.val($(this).attr('rel'));
-      $list.hide();
-      //console.log($this.val());
-  });
-
-  $(document).click(function() {
-      $styledSelect.removeClass('active');
-      $list.hide();
-  });
-
-});
-
-var galleryThumbs = new Swiper('.gallery-thumbs-two', {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-  direction: 'vertical',
-  slideActiveClass: 'slider-about-active',
-  mousewheel: true,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
-  },
-});
-var galleryTop = new Swiper('.gallery-top-two', {
-  spaceBetween: 10,
-  slidesPerView: 1,
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true
-  },
-  breakpoints: {
-    1250: {
-      direction: 'vertical',
+  if(document.querySelector('.swiper-about-slide-left p img')){
+    if (w > 700) {
+      let aboutimgstart = document.querySelectorAll('.swiper-about-slide-left p img');
+      for(let i = 0; i<aboutimgstart.length; i++){
+         document.querySelector('.swiper-about-slide-img').appendChild(aboutimgstart[i])
+      }
+    }
+  }
+ 
+  var mySwiper = new Swiper('.swiper-one', {
+    speed: 400,
+    spaceBetween: 100,
+    navigation: {
+      nextEl: '.swiper-one-next',
+      prevEl: '.swiper-one-prev',
     },
-  },
-  navigation: {
-    nextEl: '.swiper-button-end-next',
-    prevEl: '.swiper-button-end-prev',
-  },
-  thumbs: {
-    swiper: galleryThumbs
-  }
-});
+    pagination: {
+      el: '.swiper-one-pagination',
+      type: 'progressbar',
+    },
+  });
+  var mySwiper = new Swiper('.swiper-three', {
+    speed: 400,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    pagination: {
+      el: '.swiper-three-pagination',
+      type: 'progressbar',
+    },
+    breakpoints: {
+     725: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+  var galleryThumbs = new Swiper('.gallery-thumbs-one', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    direction: 'vertical',
+    slideActiveClass: 'gallery-thumbs-one-active',
+  
+  });
+  var galleryTop = new Swiper('.gallery-top-one', {
+    spaceBetween: 10,
+    effect: 'fade',
+  
+    fadeEffect: {
+      crossFade: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-women-next',
+      prevEl: '.swiper-button-women-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
+  let inp = document.querySelector('#phone');
+  inp.addEventListener('focus', _ => {
+    if(!/^\+\d*$/.test(inp.value))
+      inp.value = '+380';
+  });
+  
+  inp.addEventListener('keypress', e => {
+    if(!/\d/.test(e.key))
+      e.preventDefault();
+  });
+  var swiper = new Swiper('.swiper-about', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    freeMode: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    mousewheel: true,
+  });
+  
+  $('select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+  
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+  
+  });
+  
+  var galleryThumbs = new Swiper('.gallery-thumbs-two', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    direction: 'vertical',
+    slideActiveClass: 'slider-about-active',
+    mousewheel: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+  });
+  var galleryTop = new Swiper('.gallery-top-two', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+    breakpoints: {
+      1250: {
+        direction: 'vertical',
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-end-next',
+      prevEl: '.swiper-button-end-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
+  
+};
---
+++ yumy/js/main.js
@@ -0,0 +1,521 @@
+window.onload = function() {
+  var mySwiper = new Swiper('.swiper-container-lending', {
+    speed: 400,
+    spaceBetween: 0,
+    slidesPerView: 1,
+    lazy: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        820: {
+            width: 550,
+            height: 310,
+        },
+      }
+});
+
+var mySwiper = new Swiper('.cards-slider', {
+  speed: 400,
+  spaceBetween: 0,
+  slidesPerView: 1,
+  lazy: true,
+  width: 380,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      820: {
+          width: 400,
+          height: 208,
+      },
+    }
+});
+var mySwiper = new Swiper('.cards-slider-wrap', {
+  speed: 400,
+  spaceBetween: 10,
+  slidesPerView: 1,
+  lazy: true,
+  width: 1700,
+  navigation: {
+    nextEl: '.swiper-button-next-home',
+    prevEl: '.swiper-button-prev-home',
+  },
+  pagination: {
+    el: '.swiper-pagination-home',
+    type: 'bullets',
+  },
+    breakpoints: {
+      1800: {
+        slidesPerView: 4,
+      },
+      1500: {
+        slidesPerView: 4,
+        width: 1400,
+      },
+      1305: {
+        slidesPerView: 3,
+        width: 1200,
+
+      },
+      1100: {
+        slidesPerView: 3,
+        width: 1000,
+
+      },
+      890: {
+        slidesPerView: 2,
+        width: 800,
+      },
+      697: {
+        slidesPerView: 2,
+        width: 600,
+      },
+      480: {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        width: 450,
+      },
+      1: {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        width: 310,
+      },
+    }
+});
+  
+
+    $('.popap').click(function () {
+        $('body').css({'overflow': 'hidden'}) 
+        $(".popap-content").fadeIn(800);
+        $(".main-wrap").fadeIn(700);
+        $(".mob-menu").removeClass('show');
+        let title = $(this).attr("data-title");
+        let content = $(this).attr("data-content");
+        let infoBtn = $(this).attr("data-btn");
+        let info = $(this).attr("data-info");
+        $('.new-title').html(title);
+        $('.new-content').html(content);
+        $('.new-submit').val(infoBtn);
+        $('.input-info').val(info);
+
+        });
+        $('.popap-content-close').click(function () {
+          $('body').attr('style', '');
+            $(".main-wrap").fadeOut(800);
+            $(".popap-content").fadeOut(700);
+            });
+        $('.btn-mob-menu').click(function(){
+            $('.btn-mob-menu-line').addClass('btn-mob-menu-line-active');
+            $('.mob-menu').fadeIn(700);
+        })
+        $('.mob-close').click(function(){
+            $('.mob-menu').fadeOut(700);
+            $('.btn-mob-menu-line').removeClass('btn-mob-menu-line-active');
+        })
+        $('.left-blog-header-filter-item').click(function(){
+          $('.left-blog-header-filter').toggle(700)
+        })
+        $('.sub-menu').mouseenter(function(){
+          $(this).children().fadeIn(500)
+        })
+        $('.sub-menu').mouseleave(function(){
+          $('.sub-menu ul').delay(1000).fadeOut(500);
+        })
+      if(this.document.querySelector('#myVideo')){
+        var options = {
+          url: "https://player.vimeo.com/368183969",
+          portrait: false,
+          title: false,
+          color: 'bd242b',
+          byline: false,
+        };
+      
+        var videoPlayer = new Vimeo.Player('myVideo', options);
+      
+        videoPlayer.on('play', function() {
+          console.log('Played the video');
+        });
+      }
+
+     $(window).scroll(function(){
+      let st = $(this).scrollTop();
+      $('*[data-type="parallax"]').css({
+        "transform" : "translate(0%, " + st / 80 + "%"
+      })
+     })
+
+
+     var $page = $('html, body');
+     $('a[href*="#"]').click(function() {
+         $page.animate({
+             scrollTop: $($.attr(this, 'href')).offset().top
+         }, 400);
+         return false;
+     });
+
+
+
+     $('select').each(function(){
+      var $this = $(this), numberOfOptions = $(this).children('option').length;
+    
+      $this.addClass('select-hidden'); 
+      $this.wrap('<div class="select"></div>');
+      $this.after('<div class="select-styled"></div>');
+  
+      var $styledSelect = $this.next('div.select-styled');
+      $styledSelect.text($this.children('option').eq(0).text());
+    
+      var $list = $('<ul />', {
+          'class': 'select-options'
+      }).insertAfter($styledSelect);
+    
+      for (var i = 0; i < numberOfOptions; i++) {
+          $('<li />', {
+              text: $this.children('option').eq(i).text(),
+              rel: $this.children('option').eq(i).val()
+          }).appendTo($list);
+      }
+    
+      var $listItems = $list.children('li');
+    
+      $styledSelect.click(function(e) {
+          e.stopPropagation();
+          $('div.select-styled.active').not(this).each(function(){
+              $(this).removeClass('active').next('ul.select-options').hide();
+          });
+          $(this).toggleClass('active').next('ul.select-options').toggle();
+      });
+    
+      $listItems.click(function(e) {
+          e.stopPropagation();
+          $styledSelect.text($(this).text()).removeClass('active');
+          $this.val($(this).attr('rel'));
+          $list.hide();
+          //console.log($this.val());
+      });
+    
+      $(document).click(function() {
+          $styledSelect.removeClass('active');
+          $list.hide();
+      });
+  
+  });
+
+ let test = $('.header-animate-item');
+ let li_array = $.makeArray(test);
+let textVis = $('.header-animate');
+ var current = 0;
+setInterval(function(){
+      let text = $(li_array[current]).text();
+      textVis.text(text)
+      current++;
+      if(current >= li_array.length) current = 0;
+}, 3000);
+
+
+
+
+
+  $('.faq-item  h3').click(function(e) {
+    let $this = $(this);
+
+
+
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+        $this.removeClass('faq-header-active')
+    } else {
+      $this.parent().parent().find('.faq-content').removeClass('show');
+      $this.parent().parent().find('.faq-item  h3').removeClass('faq-header-active');
+        $this.parent().parent().find('.faq-content').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+        $this.addClass('faq-header-active')
+    }
+});
+
+
+$(".tab_item").not(":first").hide();
+$(".books .tab-title").hover(function() {
+    $(".books .tab-title").removeClass("red").eq($(this).index()).addClass("red");
+    $(".tab_item").hide().eq($(this).index()).fadeIn(100)
+}).eq(0).addClass("red");
+
+
+$('.vacancies-popap').click(function(){
+  $('body').css({'overflow': 'hidden'}) 
+  $(".main-wrap").fadeIn(700);
+  $(".vacancies-popap-content").fadeIn(800);
+})
+
+$('.vacancies-popap-close').click(function(){
+  $('body').css({'overflow': 'auto'}) 
+  $(".main-wrap").fadeOut(800);
+  $(".vacancies-popap-content").fadeOut(700);
+})
+
+
+
+
+
+
+
+
+	  // Browser supports HTML5 multiple file?
+    var multipleSupport = typeof $('<input/>')[0].multiple !== 'undefined',
+    isIE = /msie/i.test( navigator.userAgent );
+
+$.fn.customFile = function() {
+
+  return this.each(function() {
+
+    var $file = $(this).addClass('custom-file-upload-hidden'), // the original file input
+        $wrap = $('<div class="file-upload-wrapper">'),
+        $input = $('<input type="text" class="file-upload-input" />'),
+        // Button that will be used in non-IE browsers
+        $button = $('<button type="button" class="file-upload-button">Выбрать файлы</button>'),
+        // Hack for IE
+        $label = $('<label class="file-upload-button" for="'+ $file[0].id +'">Выбрать файлы</label>');
+
+    // Hide by shifting to the left so we
+    // can still trigger events
+    $file.css({
+      position: 'absolute',
+      left: '-9999px'
+    });
+
+    $wrap.insertAfter( $file )
+      .append( $file, $input, ( isIE ? $label : $button ) );
+
+    // Prevent focus
+    $file.attr('tabIndex', -1);
+    $button.attr('tabIndex', -1);
+
+    $button.click(function () {
+      $file.focus().click(); // Open dialog
+    });
+
+    $file.change(function() {
+
+      var files = [], fileArr, filename;
+
+      // If multiple is supported then extract
+      // all filenames from the file array
+      if ( multipleSupport ) {
+        fileArr = $file[0].files;
+        for ( var i = 0, len = fileArr.length; i < len; i++ ) {
+          files.push( fileArr[i].name );
+        }
+        filename = files.join(', ');
+
+      // If not supported then just take the value
+      // and remove the path to just show the filename
+      } else {
+        filename = $file.val().split('\\').pop();
+      }
+
+      $input.val( filename ) // Set the value
+        .attr('title', filename) // Show filename in title tootlip
+        .focus(); // Regain focus
+
+    });
+
+    $input.on({
+      blur: function() { $file.trigger('blur'); },
+      keydown: function( e ) {
+        if ( e.which === 13 ) { // Enter
+          if ( !isIE ) { $file.trigger('click'); }
+        } else if ( e.which === 8 || e.which === 46 ) { // Backspace & Del
+          // On some browsers the value is read-only
+          // with this trick we remove the old input and add
+          // a clean clone with all the original events attached
+          $file.replaceWith( $file = $file.clone( true ) );
+          $file.trigger('change');
+          $input.val('');
+        } else if ( e.which === 9 ){ // TAB
+          return;
+        } else { // All other keys
+          return false;
+        }
+      }
+    });
+
+  });
+
+};
+
+// Old browser fallback
+if ( !multipleSupport ) {
+  $( document ).on('change', 'input.customfile', function() {
+
+    var $this = $(this),
+        // Create a unique ID so we
+        // can attach the label to the input
+        uniqId = 'customfile_'+ (new Date()).getTime(),
+        $wrap = $this.parent(),
+
+        // Filter empty input
+        $inputs = $wrap.siblings().find('.file-upload-input')
+          .filter(function(){ return !this.value }),
+
+        $file = $('<input type="file" id="'+ uniqId +'" name="'+ $this.attr('name') +'"/>');
+
+    // 1ms timeout so it runs after all other events
+    // that modify the value have triggered
+    setTimeout(function() {
+      // Add a new input
+      if ( $this.val() ) {
+        // Check for empty fields to prevent
+        // creating new inputs when changing files
+        if ( !$inputs.length ) {
+          $wrap.after( $file );
+          $file.customFile();
+        }
+      // Remove and reorganize inputs
+      } else {
+        $inputs.parent().remove();
+        // Move the input so it's always last on the list
+        $wrap.appendTo( $wrap.parent() );
+        $wrap.find('input').focus();
+      }
+    }, 1);
+
+  });
+}	  // Browser supports HTML5 multiple file?
+		  var multipleSupport = typeof $('<input/>')[0].multiple !== 'undefined',
+		      isIE = /msie/i.test( navigator.userAgent );
+
+		  $.fn.customFile = function() {
+
+		    return this.each(function() {
+
+		      var $file = $(this).addClass('custom-file-upload-hidden'), // the original file input
+		          $wrap = $('<div class="file-upload-wrapper">'),
+		          $input = $('<input type="text" class="file-upload-input" />'),
+		          // Button that will be used in non-IE browsers
+		          $button = $('<button type="button" class="file-upload-button">Загрузить файлы</button>'),
+		          // Hack for IE
+		          $label = $('<label class="file-upload-button" for="'+ $file[0].id +'">Загрузить файлы</label>');
+
+		      // Hide by shifting to the left so we
+		      // can still trigger events
+		      $file.css({
+		        position: 'absolute',
+		        left: '-9999px'
+		      });
+
+		      $wrap.insertAfter( $file )
+		        .append( $file, $input, ( isIE ? $label : $button ) );
+
+		      // Prevent focus
+		      $file.attr('tabIndex', -1);
+		      $button.attr('tabIndex', -1);
+
+		      $button.click(function () {
+		        $file.focus().click(); // Open dialog
+		      });
+
+		      $file.change(function() {
+
+		        var files = [], fileArr, filename;
+
+		        // If multiple is supported then extract
+		        // all filenames from the file array
+		        if ( multipleSupport ) {
+		          fileArr = $file[0].files;
+		          for ( var i = 0, len = fileArr.length; i < len; i++ ) {
+		            files.push( fileArr[i].name );
+		          }
+		          filename = files.join(', ');
+
+		        // If not supported then just take the value
+		        // and remove the path to just show the filename
+		        } else {
+		          filename = $file.val().split('\\').pop();
+		        }
+
+		        $input.val( filename ) // Set the value
+		          .attr('title', filename) // Show filename in title tootlip
+		          .focus(); // Regain focus
+
+		      });
+
+		      $input.on({
+		        blur: function() { $file.trigger('blur'); },
+		        keydown: function( e ) {
+		          if ( e.which === 13 ) { // Enter
+		            if ( !isIE ) { $file.trigger('click'); }
+		          } else if ( e.which === 8 || e.which === 46 ) { // Backspace & Del
+		            // On some browsers the value is read-only
+		            // with this trick we remove the old input and add
+		            // a clean clone with all the original events attached
+		            $file.replaceWith( $file = $file.clone( true ) );
+		            $file.trigger('change');
+		            $input.val('');
+		          } else if ( e.which === 9 ){ // TAB
+		            return;
+		          } else { // All other keys
+		            return false;
+		          }
+		        }
+		      });
+
+		    });
+
+		  };
+
+		  // Old browser fallback
+		  if ( !multipleSupport ) {
+		    $( document ).on('change', 'input.customfile', function() {
+
+		      var $this = $(this),
+		          // Create a unique ID so we
+		          // can attach the label to the input
+		          uniqId = 'customfile_'+ (new Date()).getTime(),
+		          $wrap = $this.parent(),
+
+		          // Filter empty input
+		          $inputs = $wrap.siblings().find('.file-upload-input')
+		            .filter(function(){ return !this.value }),
+
+		          $file = $('<input type="file" id="'+ uniqId +'" name="'+ $this.attr('name') +'"/>');
+
+		      // 1ms timeout so it runs after all other events
+		      // that modify the value have triggered
+		      setTimeout(function() {
+		        // Add a new input
+		        if ( $this.val() ) {
+		          // Check for empty fields to prevent
+		          // creating new inputs when changing files
+		          if ( !$inputs.length ) {
+		            $wrap.after( $file );
+		            $file.customFile();
+		          }
+		        // Remove and reorganize inputs
+		        } else {
+		          $inputs.parent().remove();
+		          // Move the input so it's always last on the list
+		          $wrap.appendTo( $wrap.parent() );
+		          $wrap.find('input').focus();
+		        }
+		      }, 1);
+
+		    });
+		  }
+
+
+
+      $('input[type=file]').customFile();
+
+
+
+
+ };
